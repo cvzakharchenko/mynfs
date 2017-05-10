8d299efd59f5c6b461de9537e277ecb2ad2fe514
cvzakharchenko <cvzakharchenko@gmail.com>
2017-05-10 08:37:11 +0300

    Added passthrough example from fuse

---
+++ passthrough.cpp
@@ -0,0 +1,582 @@
+/*
+  FUSE: Filesystem in Userspace
+  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+  Copyright (C) 2011       Sebastian Pipping <sebastian@pipping.org>
+
+  This program can be distributed under the terms of the GNU GPL.
+  See the file COPYING.
+*/
+
+/** @file
+ *
+ * This file system mirrors the existing file system hierarchy of the
+ * system, starting at the root file system. This is implemented by
+ * just "passing through" all requests to the corresponding user-space
+ * libc functions. Its performance is terrible.
+ *
+ * Compile with
+ *
+ *     gcc -Wall passthrough.c `pkg-config fuse3 --cflags --libs` -o passthrough
+ *
+ * ## Source code ##
+ * \include passthrough.c
+ */
+
+
+#define FUSE_USE_VERSION 30
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+/* For pread()/pwrite()/utimensat() */
+#define _XOPEN_SOURCE 700
+#endif
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
+
+#include <fstream>
+#include <string>
+
+
+void log_str(std::string s)
+{
+	std::string path = "/home/cvz/pro/nfs/mynfs/run.log";
+	std::ofstream log_file(path, std::ofstream::app);
+	log_file << s << std::endl;
+	log_file.close();
+}
+
+std::string map_path(std::string path)
+{
+	static const std::string root_dir = "/home/cvz/pro/nfs/mynfs/local";
+	const std:: string new_path = root_dir + path;
+
+	return new_path;
+}
+
+static void *xmp_init(struct fuse_conn_info *conn,
+		      struct fuse_config *cfg)
+{
+	(void) conn;
+	cfg->use_ino = 1;
+
+	/* Pick up changes from lower filesystem right away. This is
+	   also necessary for better hardlink support. When the kernel
+	   calls the unlink() handler, it does not know the inode of
+	   the to-be-removed entry and can therefore not invalidate
+	   the cache of the associated inode - resulting in an
+	   incorrect st_nlink value being reported for any remaining
+	   hardlinks to this inode. */
+	cfg->entry_timeout = 0;
+	cfg->attr_timeout = 0;
+	cfg->negative_timeout = 0;
+
+	return NULL;
+}
+
+static int xmp_getattr(const char *path, struct stat *stbuf,
+		       struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+
+	(void) fi;
+	int res;
+
+	res = lstat(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_access(const char *path, int mask)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+
+	log_str(path);
+	int res;
+
+	res = access(path, mask);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readlink(const char *path, char *buf, size_t size)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res;
+
+	res = readlink(path, buf, size - 1);
+	if (res == -1)
+		return -errno;
+
+	buf[res] = '\0';
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi,
+		       enum fuse_readdir_flags flags)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+	(void) flags;
+
+	dp = opendir(path);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		if (filler(buf, de->d_name, &st, 0, (fuse_fill_dir_flags)0))
+			break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+
+	int res;
+
+	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
+	   is more portable */
+	if (S_ISREG(mode)) {
+		res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if (res >= 0)
+			res = close(res);
+	} else if (S_ISFIFO(mode))
+		res = mkfifo(path, mode);
+	else
+		res = mknod(path, mode, rdev);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_mkdir(const char *path, mode_t mode)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res;
+
+	res = mkdir(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_unlink(const char *path)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res;
+
+	res = unlink(path);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_rmdir(const char *path)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res;
+
+	res = rmdir(path);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_symlink(const char *from, const char *to)
+{	
+	int res;
+
+	res = symlink(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_rename(const char *from, const char *to, unsigned int flags)
+{
+	int res;
+
+	if (flags)
+		return -EINVAL;
+
+	res = rename(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_link(const char *from, const char *to)
+{	
+	int res;
+
+	res = link(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_chmod(const char *path, mode_t mode,
+		     struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	(void) fi;
+	int res;
+
+	res = chmod(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_chown(const char *path, uid_t uid, gid_t gid,
+		     struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	(void) fi;
+	int res;
+
+	res = lchown(path, uid, gid);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_truncate(const char *path, off_t size,
+			struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res;
+
+	if (fi != NULL)
+		res = ftruncate(fi->fh, size);
+	else
+		res = truncate(path, size);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+#ifdef HAVE_UTIMENSAT
+static int xmp_utimens(const char *path, const struct timespec ts[2],
+		       struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	(void) fi;
+	int res;
+
+	/* don't use utime/utimes since they follow symlinks */
+	res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+#endif
+
+static int xmp_create(const char *path, mode_t mode,
+		      struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res;
+
+	res = open(path, fi->flags, mode);
+	if (res == -1)
+		return -errno;
+
+	fi->fh = res;
+	return 0;
+}
+
+static int xmp_open(const char *path, struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res;
+
+	res = open(path, fi->flags);
+	if (res == -1)
+		return -errno;
+
+	fi->fh = res;
+	return 0;
+}
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int fd;
+	int res;
+
+	if(fi == NULL)
+		fd = open(path, O_RDONLY);
+	else
+		fd = fi->fh;
+	
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	if(fi == NULL)
+		close(fd);
+	return res;
+}
+
+static int xmp_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int fd;
+	int res;
+
+	(void) fi;
+	if(fi == NULL)
+		fd = open(path, O_WRONLY);
+	else
+		fd = fi->fh;
+	
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	if(fi == NULL)
+		close(fd);
+	return res;
+}
+
+static int xmp_statfs(const char *path, struct statvfs *stbuf)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res;
+
+	res = statvfs(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_release(const char *path, struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	(void) path;
+	close(fi->fh);
+	return 0;
+}
+
+static int xmp_fsync(const char *path, int isdatasync,
+		     struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	/* Just a stub.	 This method is optional and can safely be left
+	   unimplemented */
+
+	(void) path;
+	(void) isdatasync;
+	(void) fi;
+	return 0;
+}
+
+#ifdef HAVE_POSIX_FALLOCATE
+static int xmp_fallocate(const char *path, int mode,
+			off_t offset, off_t length, struct fuse_file_info *fi)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int fd;
+	int res;
+
+	(void) fi;
+
+	if (mode)
+		return -EOPNOTSUPP;
+
+	if(fi == NULL)
+		fd = open(path, O_WRONLY);
+	else
+		fd = fi->fh;
+	
+	if (fd == -1)
+		return -errno;
+
+	res = -posix_fallocate(fd, offset, length);
+
+	if(fi == NULL)
+		close(fd);
+	return res;
+}
+#endif
+
+#ifdef HAVE_SETXATTR
+/* xattr operations are optional and can safely be left unimplemented */
+static int xmp_setxattr(const char *path, const char *name, const char *value,
+			size_t size, int flags)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res = lsetxattr(path, name, value, size, flags);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+
+static int xmp_getxattr(const char *path, const char *name, char *value,
+			size_t size)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res = lgetxattr(path, name, value, size);
+	if (res == -1)
+		return -errno;
+	return res;
+}
+
+static int xmp_listxattr(const char *path, char *list, size_t size)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res = llistxattr(path, list, size);
+	if (res == -1)
+		return -errno;
+	return res;
+}
+
+static int xmp_removexattr(const char *path, const char *name)
+{
+	std::string new_path = map_path(path);
+	path = new_path.c_str();
+	
+	int res = lremovexattr(path, name);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+#endif /* HAVE_SETXATTR */
+
+static struct fuse_operations xmp_oper;
+
+
+int main(int argc, char *argv[])
+{
+	xmp_oper.init           = xmp_init;
+	xmp_oper.getattr	= xmp_getattr;
+	xmp_oper.access		= xmp_access;
+	xmp_oper.readlink	= xmp_readlink;
+	xmp_oper.readdir	= xmp_readdir;
+	xmp_oper.mknod		= xmp_mknod;
+	xmp_oper.mkdir		= xmp_mkdir;
+	xmp_oper.symlink	= xmp_symlink;
+	xmp_oper.unlink		= xmp_unlink;
+	xmp_oper.rmdir		= xmp_rmdir;
+	xmp_oper.rename		= xmp_rename;
+	xmp_oper.link		= xmp_link;
+	xmp_oper.chmod		= xmp_chmod;
+	xmp_oper.chown		= xmp_chown;
+	xmp_oper.truncate	= xmp_truncate;
+#ifdef HAVE_UTIMENSAT
+	xmp_oper.utimens	= xmp_utimens;
+#endif
+	xmp_oper.open		= xmp_open;
+	xmp_oper.create 	= xmp_create;
+	xmp_oper.read		= xmp_read;
+	xmp_oper.write		= xmp_write;
+	xmp_oper.statfs		= xmp_statfs;
+	xmp_oper.release	= xmp_release;
+	xmp_oper.fsync		= xmp_fsync;
+#ifdef HAVE_POSIX_FALLOCATE
+	xmp_oper.fallocate	= xmp_fallocate;
+#endif
+#ifdef HAVE_SETXATTR
+	xmp_oper.setxattr	= xmp_setxattr;
+	xmp_oper.getxattr	= xmp_getxattr;
+	xmp_oper.listxattr	= xmp_listxattr;
+	xmp_oper.removexattr	= xmp_removexattr;
+#endif
+
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
+}
